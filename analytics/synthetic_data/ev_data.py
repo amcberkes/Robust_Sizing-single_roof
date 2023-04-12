eceb3975295db7296ee6468de11723d32c611b71
amcberkes <anais.berkes@gmail.com>
2023-04-12 15:04:44 +0200

    deleted redundant stuff

---
+++ analytics/synthetic_data/ev_data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+'''
+We need to generate a list of the following type:
+t_start, t_end, soc_arrival
+
+for a period  of one year 365 days.
+
+1 - Generate functions for 24h and let it run 365 days. distinguish between weekdays and weekends, 
+so have different fucntions for that.
+
+2 - for each WFH type, run the functions 365 times to get a time series, by putting in the corresponding parameters.
+
+'''
+
+
+# function for arrival time 
+
+
+
+# function for departure time, dependent on arrval
+
+
+# function for SOC
+
+
+# take all 3 vars together and save them in the file 
